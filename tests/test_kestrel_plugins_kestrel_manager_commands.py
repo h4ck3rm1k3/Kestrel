fe2580230f5c2db12b20a9cb36a9442c83996d6c
James Michael DuPont <JamesMikeDuPont@gmail.com>
2014-08-18 09:04:59 -0500

    more tests

--- tests/test_kestrel_plugins_kestrel_manager_commands.py
+++ tests/test_kestrel_plugins_kestrel_manager_commands.py
@@ -1,7 +1,14 @@
 import unittest
 from kestrel.plugins.kestrel_manager.commands import (
     AdhocCommand, 
-    cmd_canceljob)
+    cmd_canceljob,
+    cmd_jobstatus,
+    cmd_joinpool,
+    cmd_submitjob,
+    cmd_poolstatus,
+    
+
+)
 
 session = {
     'from' : "bla"
@@ -30,64 +37,60 @@ class TestAdhocCommand(unittest.TestCase):
     def test_session_start(self):
         self.assertEqual(expected, self.adhoc_command.session_start(payload, session))
 
-
     def test_start(self):
-
         self.assertEqual(expected, self.adhoc_command.start(payload, session))
 
 
 class TestCmdPoolstatus(unittest.TestCase):
-    def test_command_init(self):
-        cmd_poolstatus = cmd_poolstatus()
-        self.assertEqual(expected, cmd_poolstatus.command_init())
+    def setUp(self):
+        self.cmd = cmd_poolstatus(config)
 
+    def test_command_init(self):
+        self.assertEqual(expected, self.cmd.command_init())
 
     def test_start(self):
-        cmd_poolstatus = cmd_poolstatus()
-        self.assertEqual(expected, cmd_poolstatus.start(form, session))
+        self.assertEqual(expected, self.cmd.start(form, session))
 
 
 class TestCmdJoinpool(unittest.TestCase):
-    def test_command_init(self):
-        cmd_joinpool = cmd_joinpool()
-        self.assertEqual(expected, cmd_joinpool.command_init())
+    def setUp(self):
+        self.cmd =  cmd_joinpool(config)
 
+    def test_command_init(self):
+        self.assertEqual(expected, self.cmd.command_init())
 
     def test_complete(self):
-        cmd_joinpool = cmd_joinpool()
-        self.assertEqual(expected, cmd_joinpool.complete(form, session))
-
+        self.assertEqual(expected, self.cmd.complete(form, session))
 
     def test_start(self):
-        cmd_joinpool = cmd_joinpool()
-        self.assertEqual(expected, cmd_joinpool.start(form, session))
+        self.assertEqual(expected, self.cmd.start(form, session))
 
 
 class TestCmdSubmitjob(unittest.TestCase):
-    def test_command_init(self):
-        cmd_submitjob = cmd_submitjob()
-        self.assertEqual(expected, cmd_submitjob.command_init())
 
+    def setUp(self):
+        self.cmd =  cmd_submitjob(config)
+
+    def test_command_init(self):
+        self.assertEqual(expected, self.cmd.command_init())
 
     def test_complete(self):
-        cmd_submitjob = cmd_submitjob()
-        self.assertEqual(expected, cmd_submitjob.complete(form, session))
-
+        self.assertEqual(expected, self.cmd.complete(form, session))
 
     def test_start(self):
-        cmd_submitjob = cmd_submitjob()
-        self.assertEqual(expected, cmd_submitjob.start(form, session))
+        self.assertEqual(expected, self.cmd.start(form, session))
 
 
 class TestCmdJobstatus(unittest.TestCase):
-    def test_command_init(self):
-        cmd_jobstatus = cmd_jobstatus()
-        self.assertEqual(expected, cmd_jobstatus.command_init())
+    def setUp(self):
+        self.cmd =  cmd_jobstatus(config)
 
+    def test_command_init(self):
+        self.assertEqual(expected, self.cmd.command_init())
 
     def test_start(self):
-        cmd_jobstatus = cmd_jobstatus()
-        self.assertEqual(expected, cmd_jobstatus.start(form, session))
+
+        self.assertEqual(expected, self.cmd.start(form, session))
 
 
 class TestCmdCanceljob(unittest.TestCase):
